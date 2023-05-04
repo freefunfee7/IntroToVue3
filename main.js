5fdc73da4164158e837a3c8e66131530b0ec405b
binzhu <binzhu@trip.com>
2023-05-04 15:38:02 +0800

    L6: 事件处理

--- main.js
+++ main.js
@@ -11,10 +11,29 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green'},
-                { id: 2235, color: 'blue'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0,
+        }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        decreToCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            } else {
+                this.cart = 0
+            }
+        },
+        
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
