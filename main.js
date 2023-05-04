be19764f03fa16da525230661b9d0207beee8675
binzhu <binzhu@trip.com>
2023-05-04 14:52:49 +0800

    L4: 条件渲染

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const app = Vue.createApp({
             description: 'This is a pair of warm, fuzzy socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.google.com',
+            inStock: false,
+            inventory: 0,
+            onSale: true,
         }
     }
 })
