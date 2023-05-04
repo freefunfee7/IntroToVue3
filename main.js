8a02eda437ec9f1cd1cd4129adc2edfed3f489b5
binzhu <binzhu@trip.com>
2023-05-04 15:05:55 +0800

    L5: 列表渲染

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ const app = Vue.createApp({
             inStock: false,
             inventory: 0,
             onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ],
+            sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         }
     }
 })
