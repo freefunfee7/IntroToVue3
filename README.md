6d25abd1cc98ea8b862114b813a2eec6fed4b9f2
binzhu <binzhu@trip.com>
2023-05-04 10:37:20 +0800

    L2-创建VueApp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IntroToVue3
+
+Course Repo: https://github.com/Code-Pop/Intro-to-Vue-3
---
+++ main.js
@@ -0,0 +1,9 @@
+// const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'This is a pair of warm, fuzzy socks',
+        }
+    }
+})
