4c6bdb90f93d144da3e5399801c1db714cd50d2c
binzhu <binzhu@trip.com>
2023-05-05 14:31:24 +0800

    L8: 计算属性

--- main.js
+++ main.js
@@ -3,20 +3,23 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            brand: 'Vue Mastery New',
             description: 'This is a pair of warm, fuzzy socks',
-            image: './assets/images/socks_green.jpg',
+            // image: './assets/images/socks_green.jpg',
             url: 'https://www.google.com',
-            inStock: false,
+            // inStock: false,
             inventory: 0,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
             sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
             cart: 0,
-            activeClass: false
+            activeClass: false,
+            selectedVariant: 0,
+            onSale: true,
         }
     },
 
@@ -35,6 +38,28 @@ const app = Vue.createApp({
         
         updateImage(variantImage) {
             this.image = variantImage
+        },
+
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
         }
     }
 })
